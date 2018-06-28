ab3a152ef6d055fe349946e5326ed3fe1e5f276b
wanfang <1260582052>
2018-06-28 13:29:49 +0800

    diidi

---
+++ Chapter5/src/main/java/com/course/BasicAnnotation.java
@@ -0,0 +1,10 @@
+package com.course;
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+    @Test
+    public void testcase1(){
+        System.out.println("这是一个测试用例！");
+    }
+}
